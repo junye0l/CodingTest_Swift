199b4b50affdc0e62b15997fb69c86f3475a1634
Junyeol <junyeolkim00@gmail.com>
2024-11-21 01:00:52 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -3,7 +3,5 @@ import Foundation
 func solution(_ numbers:[Int]) -> Int {
     // 0부터 0까지 합이 45
     // 입력 배열의 합을 45에 빼주면 없는 요소들의 합이 나올 것 같음.
-    var result: Int = 0
-    result = 45 - numbers.reduce(0,+)
-    return result
+    return 45-numbers.reduce(0,+)
 }
