1141afb7ffcf014773a331ed2d4d826b10fc500f
Junyeol <junyeolkim00@gmail.com>
2025-05-06 14:45:30 +0900

    [Bronze V] Title: ??!, Time: 92 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        sc.close();
+
+        System.out.println(name + "??!");
+	}
+}
